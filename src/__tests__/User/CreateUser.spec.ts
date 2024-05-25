f00675027ba4910c542b93de9412cfc8eeb727a3
Augusto Dias <gutodiasdev@gmail.com>
2024-05-25 12:07:11 -0300

    test: ensure user create route is accessable

---
+++ src/__tests__/User/CreateUser.spec.ts
@@ -0,0 +1,9 @@
+import api from "../../lib/axios";
+
+describe("User Suite", () => {
+  it("should ensure create route is accessable", async () => {
+    const { data, status } = await api.post("/user/create");
+    expect(status).toBe(201);
+    expect(data).toEqual({ message: "ok" })
+  });
+})

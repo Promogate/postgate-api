052e5821e79d3ecefc998c1e224503365c44cf11
Augusto Dias <gutodiasdev@gmail.com>
2024-05-26 16:30:49 -0300

    feat(util): add stringifyCircularJSON method

---
+++ src/utils/stringifyCircularJSON.ts
@@ -0,0 +1,10 @@
+export const stringifyCircularJSON = (obj: object) => {
+  const seen = new WeakSet();
+  return JSON.stringify(obj, (k, v) => {
+    if (v !== null && typeof v === 'object') {
+      if (seen.has(v)) return;
+      seen.add(v);
+    }
+    return v;
+  });
+};

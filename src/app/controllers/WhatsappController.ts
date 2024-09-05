b7fb6bdd4e38bf3c2bbdfb9a122ea50704e9515c
Augusto Dias <gutodiasdev@gmail.com>
2024-09-04 21:54:37 -0300

    refact: add readonly flags in constructor params and create a whatsappApiService factory to return correct whatsapp api instance

--- src/app/controllers/WhatsappController.ts
+++ src/app/controllers/WhatsappController.ts
@@ -7,21 +7,25 @@ import prisma from "../../lib/prisma";
 import { SaveManyWhatsappChats } from "../interfaces/SaveManyWhatsappChats";
 import logger from "../../utils/logger";
 import CodechatService from "../services/CodechatService";
+import EvolutionService from "../services/EvolutionService";
+import AppError from "../../helpers/AppError";
 
 export default class WhatsappController {
   constructor(
-    httpServer: HttpServer,
-    whatsappSessionsService: WhatsappSessionsService,
-    saveManyChatsService: SaveManyWhatsappChats,
-    codechatService: CodechatService
+    readonly httpServer: HttpServer,
+    readonly whatsappSessionsService: WhatsappSessionsService,
+    readonly saveManyChatsService: SaveManyWhatsappChats,
+    readonly codechatService: CodechatService,
+    readonly evolutionService: EvolutionService
   ) {
+    const whatsappApiService = this.getWhatsappApiService();
     httpServer.on(
       "post",
       "/whatsapp/session/create",
       [verifyToken],
       async (request: Request & { user?: string }, response: Response) => {
         const body = request.body;
-        await whatsappSessionsService.createSession({
+        await whatsappApiService.connect({
           userId: request.user as string,
           name: body.name,
           description: body.description
@@ -49,12 +53,12 @@ export default class WhatsappController {
           }
           clients.forEach(async (client) => {
             if (client.status === "" || !client.status) {
-              const connectionsState = await codechatService.isInstanceConnected({
+              const connectionsState = await whatsappApiService.isInstanceConnected({
                 instanceName: client.id,
                 token: body.token
               });
               if (connectionsState.state === "open") {
-                await codechatService.updateConnectionState({ id: client.id, status: "CONNECTED" });
+                await whatsappApiService.updateConnectionState({ id: client.id, status: "CONNECTED" });
               }
             }
           });
@@ -63,45 +67,63 @@ export default class WhatsappController {
           return response.status(HttpStatusCode.BAD_REQUEST).send({ message: error.message });
         }
       });
-    httpServer.on("get", "/whatsapp/session/active-sessions", [verifyToken],
-      async (request: Request, response: Response) => {
-        const result = await whatsappSessionsService.countActiveSessions();
-        return response.json(result).status(200);
-      });
-    httpServer.on("get", "/whatsapp/get-chats/:sessionId", [verifyToken],
-      async (request: Request, response: Response) => {
-        const sessionId = request.params.sessionId as string;
-        const result = await whatsappSessionsService.getChats(sessionId);
-        return response.json(result).status(200);
-      });
-    httpServer.on("get", "/whatsapp/sync/:sessionId", [verifyToken],
-      async (request: Request, response: Response) => {
-        const sessionId = request.params.sessionId as string;
-        if (!sessionId) {
-          return response.status(HttpStatusCode.UNPROCESSABLE_ENTITY).send({ message: "Sessin ID is missing!" });
-        }
+    httpServer.on("get", "/whatsapp/qrcode/:instanceId", [verifyToken],
+      async (request: Request & { user?: string }, response: Response) => {
+        const body = request.body;
+        const { instanceId } = request.params as { instanceId: string };
         try {
-          const result = await whatsappSessionsService.getChats(sessionId);
-          await saveManyChatsService.execute({ chats: result, sessionId: sessionId });
-          return response.status(HttpStatusCode.OK).send();
+          const result = await whatsappApiService.getQRCode({ instanceName: instanceId, token: body.token });
+          return response.json(result).status(HttpStatusCode.OK);
         } catch (error: any) {
           logger.error(error.message);
-          return response.status(HttpStatusCode.BAD_REQUEST);
+          throw new AppError({ message: error.message, statusCode: HttpStatusCode.BAD_REQUEST });
         }
-      });
-    httpServer.on("delete", "/whatsapp/session/:sessionId", [verifyToken],
-      async (request: Request, response: Response) => {
-        const sessionId = request.params.sessionId as string;
-        if (!sessionId) {
-          return response.status(HttpStatusCode.UNPROCESSABLE_ENTITY).send({ message: "Sessin ID is missing!" });
-        }
-        try {
-          await prisma.whatsappSession.delete({ where: { id: sessionId } });
-          return response.status(HttpStatusCode.OK).send();
-        } catch (error: any) {
-          logger.error(error.message);
-          return response.status(HttpStatusCode.BAD_REQUEST);
-        }
-      });
+      }
+    )
+    // httpServer.on("get", "/whatsapp/session/active-sessions", [verifyToken],
+    //   async (request: Request, response: Response) => {
+    //     const result = await whatsappSessionsService.countActiveSessions();
+    //     return response.json(result).status(200);
+    //   });
+    // httpServer.on("get", "/whatsapp/get-chats/:sessionId", [verifyToken],
+    //   async (request: Request, response: Response) => {
+    //     const sessionId = request.params.sessionId as string;
+    //     const result = await whatsappSessionsService.getChats(sessionId);
+    //     return response.json(result).status(200);
+    //   });
+    // httpServer.on("get", "/whatsapp/sync/:sessionId", [verifyToken],
+    //   async (request: Request, response: Response) => {
+    //     const sessionId = request.params.sessionId as string;
+    //     if (!sessionId) {
+    //       return response.status(HttpStatusCode.UNPROCESSABLE_ENTITY).send({ message: "Sessin ID is missing!" });
+    //     }
+    //     try {
+    //       const result = await whatsappSessionsService.getChats(sessionId);
+    //       await saveManyChatsService.execute({ chats: result, sessionId: sessionId });
+    //       return response.status(HttpStatusCode.OK).send();
+    //     } catch (error: any) {
+    //       logger.error(error.message);
+    //       return response.status(HttpStatusCode.BAD_REQUEST);
+    //     }
+    //   });
+    // httpServer.on("delete", "/whatsapp/session/:sessionId", [verifyToken],
+    //   async (request: Request, response: Response) => {
+    //     const sessionId = request.params.sessionId as string;
+    //     if (!sessionId) {
+    //       return response.status(HttpStatusCode.UNPROCESSABLE_ENTITY).send({ message: "Sessin ID is missing!" });
+    //     }
+    //     try {
+    //       await prisma.whatsappSession.delete({ where: { id: sessionId } });
+    //       return response.status(HttpStatusCode.OK).send();
+    //     } catch (error: any) {
+    //       logger.error(error.message);
+    //       return response.status(HttpStatusCode.BAD_REQUEST);
+    //     }
+    //   });
+  }
+
+  private getWhatsappApiService() {
+    if (process.env.WHATSAPP_ENGINE === "codechat") return this.codechatService;
+    return this.evolutionService;
   }
 }
